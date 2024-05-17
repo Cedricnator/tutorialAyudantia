644fb51b18278a3e7f243a1664c5beeef6729661
Cedric Kirmayr <Cedric.kirmayr@gmail.com>
2024-05-17 14:56:18 -0400

    feat: directiva v-if into message component

---
+++ src/components/Message.vue
@@ -0,0 +1,13 @@
+<script>
+    export default {
+        data() {
+        return {
+            mostrarMensaje: true
+        }
+    }
+}
+</script>
+
+<template>
+    <p v-if="mostrarMensaje">Hola mundo</p>
+</template>
